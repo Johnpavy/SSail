b1705bda0d42cf263c11b5d336719d0f6f45f878
Johnpavy <johnpavelchak@yahoo.com>
2015-04-20 22:14:31 -0500

    Starting project
    Doesn't do anything yet, just have the UI and the start of user class.

---
+++ src/User.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author John
+ */
+public class User {
+    String fName;
+    String lName;
+    String password;
+    String login;
+    
+    void setFName(String f) {
+        this.fName = f;
+    }
+    
+    String getFName(){
+        return this.fName;
+    }
+    
+}
